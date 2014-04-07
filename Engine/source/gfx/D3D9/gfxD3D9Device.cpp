36ac03cd2573f92305f8faf3d1e8def3f0afd243
Anis <anishireche@gmail.com>
2014-04-07 16:23:08 +0200

    Update gfxD3D9Device.cpp

--- Engine/source/gfx/D3D9/gfxD3D9Device.cpp
+++ Engine/source/gfx/D3D9/gfxD3D9Device.cpp
@@ -139,7 +139,7 @@ D3DPRESENT_PARAMETERS GFXD3D9Device::setupPresentParams(const GFXVideoMode &mode
 	d3dpp.BackBufferWidth  = mode.resolution.x;
 	d3dpp.BackBufferHeight = mode.resolution.y;
 	d3dpp.BackBufferFormat = fmt;
-	d3dpp.BackBufferCount  = !smDisableVSync ? 2 : 1; // anis -> triple buffering to avoid tearing when vsync is off! NOTE: would be cool to set it from graphics options...
+	d3dpp.BackBufferCount  = !smDisableVSync ? 2 : 1; // anis -> triple buffering to avoid tearing when vsync is on! NOTE: would be cool to set it from graphics options...
 	d3dpp.MultiSampleType  = aatype;
 	d3dpp.MultiSampleQuality = aalevel;
 	d3dpp.SwapEffect       = /* IsWin7OrLater() ? D3DSWAPEFFECT_FLIPEX : */ D3DSWAPEFFECT_DISCARD; // NOTE: anis -> DirectX9Ex support for windows 7 or more current
